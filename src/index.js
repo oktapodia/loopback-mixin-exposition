564ab7d018998351d9d86dc8c93834f1a61b0159
BRAMILLE Sébastien <sebastien.bramille@gmail.com>
2016-08-17 14:29:06 +0100

    Upgrade babelrc

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { deprecate } from 'util';
-import MethodExposition from './MethodExposition';
+import methodExposition from './methodExposition';
 
 export default deprecate(
-  app => app.loopback.modelBuilder.mixins.define('MethodExposition', MethodExposition),
+  app => app.loopback.modelBuilder.mixins.define('MethodExposition', methodExposition),
   'DEPRECATED: Use mixinSources, see https://github.com/oktapodia/loopback-mixin-method-exposition#mixinsources'
 );
